ec06a0536a41ca4707cc34a374120b57cec85568
balloch <theincubus313@gmail.com>
2018-12-07 19:25:56 -0500

    adding grid check to perturb_json

--- scripts/perturb_json.py
+++ scripts/perturb_json.py
@@ -25,10 +25,29 @@ if sys.version_info >=(3,0):
     sys.stout.write("Python 2.x recommended, should still work with Python 3.x")
 
 
-person_modelIds = ['323','324','325','333','346','s__1779','s__1780','s__1781',
+person_modelIds =  ['323','324','325','333','346','s__1779','s__1780','s__1781',
                     's__1782','s__1783','s__1784','s__1785','s__1786','s__1787',
                     's__1788','s__1789','s__1957','s__1957','s__1957','s__1957',
                     's__1957','s__1957']
+
+# Floor='Floor', box(unkown)='Box', and the rest are real objects. List onlu through 600
+# important obj: table,stand,shelf,dresser,bed,desk,ottoman,rug
+# TODO should probably make "region of support" annotations for objects (e.g. chair, sofa,bookshelf)
+support_modelIds = ['40','41','75','77','78','104','105','107','108','109',
+                    '115','116','117','119','128','129','134','140','141','146',
+                    '147','153','171','172','173','174','188','192','193','195',
+                    '197','198','199','200','201','202','203','204','205','207',
+                    '233','235','238','252','268','270','293','294','311','312',
+                    '315','318','390','396','397','398','403','404','404','417',
+                    '424','428','430','431','440','451','452','454','455','456',
+                    '457','458','459','464','468','469','470','472','473','474',
+                    '477','478','480','483','484','485','486','487','488','489',
+                    '490','492','493','494','495','504','505','506','508','509',
+                    '513','514','525','533','534','541','542','543','544','545',
+                    '546','547','548','549','550','551','552','553','554','555',
+                    '556','557','558','559','560','561','563','564','565','566',
+                    '567','568','569','570','577','579','580','586','587','589',
+                    '590','591','592']
 
 perturbable_modelIds = []
 
@@ -45,7 +64,10 @@ def cuboid_collision(bbox1, bbox2): #Separating Axis test, https://stackoverflow
 def find_support_height(new_pose, support_grid): #projects down to support
     pass
 
-def check_real_support(bbox1, support_grid, IoU=.5): #checks if bbox is at least IoU overlapping with support bbox 
+'''
+Checks if bbox is at least IoU overlapping with support bbox and if bbox of support smaller than obj
+'''
+def check_real_support(bbox1, support_grid, IoU=.25):
     pass
 
 
@@ -68,9 +90,9 @@ def clean_json(input_file_name):
                 if node['modelId'] in person_modelIds:
                     for_deletion.append((idx_n,int(node['id'])))
             for node_idx in for_deletion:
-                for room in rooms: # TODO: can be much faster with numpy binary mask
-                    room['nodeIndices'][:] = [x for x in room['nodeIndices'] if x<1]
-                del level['nodes'][node_idx[0]] # UNITTEST: make sure this doesn't delete necessary stuff
+                for room in rooms: # TODO: can be faster with numpy binary mask
+                    room['nodeIndices'][:] = [x for x in room['nodeIndices'] if x==node_idx[1]]
+                del level['nodes'][node_idx[0]] # UNITTEST: make sure this doesn't delete necessary stuff like rooms
             print("Removed %d nodes" %len(for_deletion))
             for_deletion = []
             rooms = []
@@ -89,20 +111,19 @@ def perturb_json(input_file_name):
     house_dict={}
     with open(input_file_name, 'r') as json_if:
         house_dict = json.load(json_if)
-        rooms = {}
         for level in house_dict['levels']:
+            #thought about doing this with polygons, too hard, see https://stackoverflow.com/questions/36399381/whats-the-fastest-way-of-checking-if-a-point-is-inside-a-polygon-in-python
             rand_pert = np.random.rand(3,)
             # pert_obj = np.random.randint(7,100)
             pert_obj = 59 #television, model Id 228, id 0_59 in room 5 in sample house
             room_count = 0 #TEST
             object_count = 0 #TEST
             for node in level['nodes']:
+                rooms = []
                 if node['type'] == 'Room':
-                    room_count += 1
-                #needs a way yot tell if an object is in a room: has "nodeIndices"
+                    grid_shape = np.ceil((np.asarray(node['bbox']['max'])-np.asarray(node['bbox']['min']))[0::2]*100).astype(int)
+                    rooms.append({'data':node, 'grid':np.empty(shape=grid_shape)})
                 elif node['type'] == 'Object':
-                    if node['modelId'] in person_modelIds:
-                        print('person') #TEST
                     if int(node['id'][2:]) is pert_obj:
                         old_min=np.asarray(node['bbox']['min'])
                         old_max=np.asarray(node['bbox']['max'])
@@ -111,7 +132,7 @@ def perturb_json(input_file_name):
                         node['bbox']['min']=list(new_min)
                         node['bbox']['max']=list(new_max)
                     object_count += 1
-            print('number of rooms = ', room_count) #TEST
+            print('number of rooms = ', len(rooms)) #TEST
             print('number of objects = ', object_count) #TEST
 
     with open(input_file_name[:-5]+'_pert'+input_file_name[-5:], 'w') as json_of:
